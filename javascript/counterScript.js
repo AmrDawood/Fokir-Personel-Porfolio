48816e9947baeff9c8dd55402c6db8d1452970e1
AmrDawood-codeWebTech <amrdawood91@gmail.com>
2020-09-07 15:22:25 +0200

    light box
    slider on portfoliio section

--- javascript/counterScript.js
+++ javascript/counterScript.js
@@ -1,3 +1,64 @@
+                     // ourPorfolio section light box script
+let imgs = Array.from(document.querySelectorAll(".gallaryItem .inner img"));
+let lightBoxContainer = document.getElementById("lightBoxContainer");
+let lightBoxItem = document.getElementById("lightBoxItem");
+let nextBtn = document.getElementById("next");
+let prevBtn  = document.getElementById("prev");
+let currentSlideIndex = 0  ;
+
+for (let index = 0; index < imgs.length; index++) {
+   imgs[index].addEventListener('click',showLightBoxContainer); 
+}
+function showLightBoxContainer(eventInfo){
+    currentSlideIndex =imgs.indexOf(eventInfo.target);
+    let imgSrc = eventInfo.target.src;
+    lightBoxContainer.style.display ="flex";
+    lightBoxItem.style.backgroundImage= `url(${imgSrc})`;
+}
+
+nextBtn.addEventListener("click",nextSlide);
+function nextSlide(){
+    currentSlideIndex++;
+    if(currentSlideIndex==imgs.length){
+        currentSlideIndex = 0 ;
+    }
+    let imgSrc =imgs[currentSlideIndex].src;
+    lightBoxItem.style.backgroundImage= `url(${imgSrc})`;
+}
+
+prevBtn.addEventListener("click",prevSlide);
+function prevSlide(){
+    currentSlideIndex--;
+    if(currentSlideIndex< 0){
+        currentSlideIndex = imgs.length-1 ;
+    }
+    let imgSrc =imgs[currentSlideIndex].src;
+    lightBoxItem.style.backgroundImage= `url(${imgSrc})`;
+}
+
+document.getElementById('close').addEventListener("click",closeSlide);
+function closeSlide(){
+    lightBoxContainer.style.display ="none";
+}
+
+document.addEventListener('keydown' ,function(eventInfo){
+    if(eventInfo.keyCode==39){
+        nextSlide();
+    }else if(eventInfo.keyCode==37){
+        prevSlide();
+    }else if(eventInfo.keyCode==27){
+        closeSlide();
+    }
+})
+
+lightBoxContainer.addEventListener("click",function(eventInfo){
+
+    if(eventInfo.target !=lightBoxItem && eventInfo.target != prevBtn && eventInfo.target != nextBtn ){
+        closeSlide();
+    }
+})
+
+                     // statistics section counter script
 let counters =document.querySelectorAll(".statisticsCol span");
 let counterSpeed= 250;
 let statistics =document.getElementById('statistics');
